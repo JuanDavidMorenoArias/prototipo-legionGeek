5c6c1142b307c56767872f5cfed6c1e635c9dc20
Juan David Moreno <jmorenoar@unal.edu.co>
2025-02-25 22:24:27 -0500

    se elimino residuos en el archivo main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from app import App
-from moderator import Moderator
 
 if __name__ == "__main__":
-    moderador1=Moderator("Sebastian Estrada Villa",1035972826,3053383941,"seestradav@unal.edu.co","Sebas1234")
     App()
